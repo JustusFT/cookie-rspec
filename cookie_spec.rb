88cc97fc38d02d80a9a3845e098b6931746df19c
JustusFT <JustusFT@gmail.com>
2017-05-06 15:07:52 +0800

    solution

--- cookie_spec.rb
+++ cookie_spec.rb
@@ -23,16 +23,19 @@ describe Cookie do
   end
 
   describe "#type" do
-    it "returns the type of the cookie"
-
+    it "returns the type of the cookie" do
+      expect(cookie.type).to eq(type)
+    end
   end
 
   describe "#bake!" do
-    it "requires an integer time argument"
+    it "requires an integer time argument" do
+      expect(cookie.time_baked).to be_kind_of(Integer)
+    end
 
-
-    it "returns the cookie object"
-
+    it "returns the cookie object" do
+      expect(cookie.bake!(0)).to eq(cookie)
+    end
 
     it "changes the status of the cookie when given enough time" do
       expect { cookie.bake!(10) }.to change(cookie, :status)
@@ -40,32 +43,38 @@ describe Cookie do
   end
 
   describe "#status" do
-    it "returns the cookie's current status"
-
-
-    context "when unbaked" do
-      it "is `:doughy`"
-
+    it "returns the cookie's current status" do
+      expect(cookie.status).to eq(:doughy)
     end
+  end
 
-    context "when baked for less than 7 minutes" do
-      it "is `:doughy`"
-
+  context "when unbaked" do
+    it "is `:doughy`" do
+      expect(cookie.status).to eq(:doughy)
     end
+  end
 
-    context "when baked for at least 7 but less than 10 minutes" do
-      it "is `:almost_ready`"
-
+  context "when baked for less than 7 minutes" do
+    it "is `:doughy`" do
+      expect(cookie.bake!(6).status).to eq(:doughy)
     end
+  end
 
-    context "when baked for at least 10 but less than 12 minutes" do
-      it "is `:ready`"
-
+  context "when baked for at least 7 but less than 10 minutes" do
+    it "is `:almost_ready`" do
+      expect(cookie.bake!(9).status).to eq(:almost_ready)
     end
+  end
 
-    context "when baked for at least 12 minutes" do
-      it "is `:burned`"
+  context "when baked for at least 10 but less than 12 minutes" do
+    it "is `:ready`" do
+      expect(cookie.bake!(11).status).to eq(:ready)
+    end
+  end
 
+  context "when baked for at least 12 minutes" do
+    it "is `:burned`" do
+      expect(cookie.bake!(12).status).to eq(:burned)
     end
   end
 end
